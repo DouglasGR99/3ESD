70431c4d98e22ed39ffc7e29035224ef1ce50b61
Douglas Garcia Rangel <109640187+DouglasGR99@users.noreply.github.com>
2023-05-03 21:00:53 -0300

    craindo a main dod tad pilha

--- atividades/TAD pilha encadeada/main.c
+++ atividades/TAD pilha encadeada/main.c
@@ -2,9 +2,30 @@
 // Created by Douglas on 30/04/2023.
 //
 #include <stdio.h>
-#include <stdlib.h>
 #include "pilha.h"
 
 void main() {
+    Pilha *p = criaPilha();
+    int dado;
 
+    if (pilhaVazia(p)) { printf("Pilha vazia\n");
+    } else { printf("Pilha nao vazia\n"); }
+
+    //preenchendo a pilha
+    for (int i = 1; i < 6; ++i) { inserePilha(p, i * 10); }
+
+    if (pilhaVazia(p)) { printf("Pilha vazia\n");
+    } else { printf("Pilha nao vazia\n"); }
+
+    //consultando o topo e esvasiando a pilha
+    for (int i = 0; i < 5; ++i) {
+        consultaPilha(p, &dado);
+        printf("Dado do topo: %d\n", dado);
+        removePilha(p);
+    }
+
+    if (pilhaVazia(p)) { printf("Pilha vazia\n");
+    } else { printf("Pilha nao vazia\n"); }
+
+    liberaPilha(p);
 }
